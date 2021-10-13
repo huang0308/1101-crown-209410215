0382acee9e1e8abfe741b23cd5ee8457752c71d6
huang0308 <209410215@gms.tku.edu.tw>
2021-10-13 21:02:48 +0800

    modify package.json

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg')
 
+const isProduction = process.env.MoDE_ENV ==="production"
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'crown_15',
     password: '0000',
     port: 5432,
+    connectionString: isProduction ? process.env.DATABASE_URL :`postgresql://postgres:0000@localhost:5432/crown15`
 })
 
 pool.query('SELECT * from category_15', (err, res) => {
