90849d892947e062a497ff6aa7d90a8d7944ceb6
huang0308 <209410215@gms.tku.edu.tw>
2021-10-20 22:06:38 +0800

    new api

--- controllers/apiCrown2Controller_15.js
+++ controllers/apiCrown2Controller_15.js
@@ -2,14 +2,8 @@ const serviceCrown2Controller_15 = require('./serviceCrown2Controller_15');
 
 exports.getCategories = async (req, res) => {
     try{
-    let results = await serviceCrown2Controller_15.getCategories();
-    console.log('results',JSON.stringify(results));
-    res.render('crown2_15',{
-      data: results,  
-      title: 'crown2_15',
-      name:'Tzujia Huang',
-      id:`209410215`
-    })
+    const data = await serviceCrown2Controller_15.getCategories();
+    return res.json(data);
 
     }catch(err){
         console.log('crown2Controller getCategories',err);
